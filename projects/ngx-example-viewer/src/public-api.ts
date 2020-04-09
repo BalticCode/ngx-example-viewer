bbd116e9ac33d71e8e5638a02b039594ca3de32c
Basti Hoffmann <bo.hoffi@gmail.com>
2020-04-09 18:20:59 +0200

    chore: initial setup

---
+++ projects/ngx-example-viewer/src/public-api.ts
@@ -0,0 +1,4 @@
+/*
+ * Public API Surface of ngx-example-viewer
+ */
+export * from './lib/ngx-example-viewer.module';
---
+++ schematics/src/utils/schematics.consts.ts
@@ -0,0 +1,2 @@
+export const PROJECT_NAME = '@balticcode/ngx-example-viewer';
+export const LIB_NAME = '@balticcode/ngx-example-viewer';
