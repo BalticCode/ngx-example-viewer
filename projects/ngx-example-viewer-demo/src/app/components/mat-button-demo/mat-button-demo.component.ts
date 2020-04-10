8f44c2c0f862695db7084e54b2417553aca556e8
Basti Hoffmann <bo.hoffi@gmail.com>
2020-04-10 22:33:36 +0200

    chore: delete old demo code

--- projects/ngx-example-viewer-demo/src/app/components/mat-button-demo/mat-button-demo.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mat-button-demo',
-  templateUrl: './mat-button-demo.component.html',
-  styleUrls: ['./mat-button-demo.component.scss']
-})
-export class MatButtonDemoComponent { }
