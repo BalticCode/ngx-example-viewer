e1c4d165614464a1e31086977a62b911abead830
Basti Hoffmann <bo.hoffi@gmail.com>
2020-04-10 22:33:58 +0200

    chore: add demo docs

---
+++ projects/ngx-example-viewer-demo/src/app/docs/mat-button-demo/mat-button-demo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-button-demo',
+  templateUrl: './mat-button-demo.component.html',
+  styleUrls: ['./mat-button-demo.component.scss']
+})
+export class MatButtonDemoComponent { }
