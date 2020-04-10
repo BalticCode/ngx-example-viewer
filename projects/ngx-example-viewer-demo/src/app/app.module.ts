be3bf6b75ffa6cdee5e8f0b54350a88f88c523b7
Basti Hoffmann <bo.hoffi@gmail.com>
2020-04-10 22:34:29 +0200

    chore: clean up

--- projects/ngx-example-viewer-demo/src/app/app.module.ts
+++ projects/ngx-example-viewer-demo/src/app/app.module.ts
@@ -1,26 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/root/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ExampleViewerModule } from '@balticcode/ngx-example-viewer';
-import { DemoExampleComponent } from './components/demo-example/demo-example.component';
-import { MatButtonDemoComponent } from './components/mat-button-demo/mat-button-demo.component';
+import { MatButtonDemoComponent } from './docs/mat-button-demo/mat-button-demo.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoExampleComponent,
     MatButtonDemoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     BrowserAnimationsModule,
+    RouterModule.forRoot([], {
+      anchorScrolling: 'enabled'
+    }),
 
     ExampleViewerModule,
     MatButtonModule,
